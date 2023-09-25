e64c12baff659ada8f41f668d90bc5691354b18a
developernkya <developernkya@gmail.com>
2023-09-25 15:08:32 +0300

    20230925 | LOGIN LOGIC AND DASHBOARD

--- app/src/main/java/com/appsnipp/creativelogindesigns/LoginActivity.java
+++ app/src/main/java/com/appsnipp/creativelogindesigns/LoginActivity.java
@@ -1,36 +1,81 @@
 package com.appsnipp.creativelogindesigns;
 
+import static android.content.ContentValues.TAG;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class LoginActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_login);
         setContentView(R.layout.activity_login);
 
+        mAuth = FirebaseAuth.getInstance();
+
+
 //        getting the values::
-        TextView email = (TextView) findViewById(R.id.editTextEmail);
-        TextView password = (TextView) findViewById(R.id.editTextPassword);
+//        TextView email = (TextView) findViewById(R.id.editTextEmail);
+//        TextView password = (TextView) findViewById(R.id.editTextPassword);
         TextView navSignup = (TextView) findViewById(R.id.move_to_signup);
 
+
+        //The values:
+        EditText emailEditText = (EditText) findViewById(R.id.editTextEmail);
+        EditText passwordEditText = (EditText) findViewById(R.id.editTextPassword);
 
 
         //Setting onclick listener for login button::
         Button button = (Button)findViewById(R.id.cirLoginButton);
         button.setOnClickListener(new View.OnClickListener(){
+
+            //implementing login functionality::
+
             @Override
             public void onClick(View view) {
-                Toast toast=Toast.makeText(getApplicationContext(),email.getText(),Toast.LENGTH_SHORT);
-                toast.setMargin(50,50);
-                toast.show();
+                mAuth.signInWithEmailAndPassword(emailEditText.getText().toString(), passwordEditText.getText().toString())
+                        .addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful()) {
+                                    // Sign in success, update UI with the signed-in user's information
+                                    Log.d(TAG, "signInWithEmail:success");
+                                    FirebaseUser user = mAuth.getCurrentUser();
+                                    Toast.makeText(LoginActivity.this, "Login Succesfull.",
+                                            Toast.LENGTH_SHORT).show();
+
+                                    Intent intent = new Intent(getApplicationContext(), Dashboard.class);
+                                    startActivity(intent);
+
+//                                    updateUI(user);
+                                } else {
+                                    // If sign in fails, display a message to the user.
+                                    Log.w(TAG, "signInWithEmail:failure", task.getException());
+                                    Toast.makeText(LoginActivity.this, "Authentication failed.",
+                                            Toast.LENGTH_SHORT).show();
+//                                    updateUI(null);
+                                }
+                            }
+                        });
             }
         });
 
